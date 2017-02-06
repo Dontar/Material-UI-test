86cf2d423564eb621884c864bd36cb9f519335b5
Kaloyan Arsov <kaloyan.arsov@gmail.com>
2017-02-06 17:58:06 +0200

    blah

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
-import { AppBar, IconButton } from 'material-ui';
+import { AppBar, IconButton, Tab, Tabs, FontIcon } from 'material-ui';
 import ActionPowerSettingsNew from "material-ui/svg-icons/action/power-settings-new"
 
 export class Header extends React.Component<undefined, undefined> {
+	renderTab() {
+		return (
+			<Tabs style={{ paddingTop: "16px"}}>
+				<Tab label="labala1" style={{width: "110px"}} />
+				<Tab label="labala1" style={{width: "110px"}} />
+				<Tab label="labala1" style={{width: "110px"}} />
+				<Tab label="labala1" style={{width: "110px"}} />
+			</Tabs>
+		);
+	}
 	render() {
 		return (
-			<AppBar title="My AppBar" showMenuIconButton={false} iconElementRight={<IconButton><ActionPowerSettingsNew /></IconButton>} />
+			<AppBar title="My AppBar" showMenuIconButton={false}>
+				{this.renderTab()}
+				<IconButton style={{marginTop: "10px", marginLeft: "10px"}}><ActionPowerSettingsNew /></IconButton>
+			</AppBar>
 		);
 	}
 }
